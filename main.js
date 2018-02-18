80c8713722a05d853fb5f2d353df1945a9767c84
ItsJimi <jimi.maiquez@gmail.com>
2018-02-18 22:38:17 +0100

    Add button to get url

--- main.js
+++ main.js
@@ -2,9 +2,22 @@ chrome.tabs.executeScript( {
   code: "window.getSelection().toString()"
 }, function (selection) {
   if (!selection) return document.getElementById('text').innerHTML = `Can't get selected text from this page.`
+  qrcode(selection[0])
+})
+
+document.getElementById('url').addEventListener('click', function () {
+  chrome.tabs.executeScript( {
+    code: "window.location.toString()"
+  }, function (selection) {
+    if (!selection) return document.getElementById('text').innerHTML = `Can't get url.`
+    qrcode(selection[0])
+  })
+})
+
+function qrcode (value) {
   const qr = new QRious({
     size: 200,
     element: document.getElementById('qr'),
-    value: selection[0]
+    value: value
   })
-})
+}
